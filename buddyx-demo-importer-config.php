da4acbc2034f8c0d9d2fb4fc4da05ce86377597b
Upendra Singh <pallavi@wbcomdesigns.com>
2023-12-22 15:51:45 +0530

    updated block widgets wie file

--- buddyx-demo-importer-config.php
+++ buddyx-demo-importer-config.php
@@ -106,7 +106,7 @@ function bdi_import_files() {
 			'import_preview_image_url'   => 'https://www.example.com/merlin/preview_import_image1.jpg',
 			'import_notice'              => __( 'A special note for this import.', 'buddyx-demo-Importer' ),
 			'preview_url'                => 'https://www.example.com/my-demo-1',
-			'import_plugins'             => array( 'elementor', 'classic-widgets', 'kirki', 'buddypress', 'woocommerce', 'wbcom-essential', 'groovy-menu' ),
+			'import_plugins'             => array( 'elementor', 'kirki', 'buddypress', 'woocommerce', 'wbcom-essential', 'groovy-menu' ),
 		),
 		array(
 			'import_file_name'           => 'BuddyX with BB Platform',
